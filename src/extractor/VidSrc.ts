6adfd96098139a54e9fe89f5ea37000474ec5427
Rahul6051276 <techsmart397@gmail.com>
2026-03-27 15:25:40 +0530

    Update VidSrc.ts

--- src/extractor/VidSrc.ts
+++ src/extractor/VidSrc.ts
@@ -1,7 +1,6 @@
 import * as cheerio from 'cheerio';
-import slugify from 'slugify';
-import { NotFoundError, TooManyRequestsError } from '../error';
-import { Context, CountryCode, Format, NonEmptyArray, UrlResult } from '../types';
+import { BlockedError, TooManyRequestsError } from '../error';
+import { Context, Format, InternalUrlResult, Meta, NonEmptyArray } from '../types';
 import { Fetcher, guessHeightFromPlaylist } from '../utils';
 import { Extractor } from './Extractor';
 
@@ -10,76 +9,74 @@ export class VidSrc extends Extractor {
 
   public readonly label = 'VidSrc';
 
-  private readonly fetcher: Fetcher;
-  private readonly tlds: NonEmptyArray<string>;
+  public override readonly ttl: number = 10800000; // 3h
 
-  public constructor(fetcher: Fetcher, tlds: NonEmptyArray<string>) {
-    super();
+  private readonly domains: NonEmptyArray<string>;
 
-    this.fetcher = fetcher;
-    this.tlds = tlds;
+  public constructor(fetcher: Fetcher, domains: NonEmptyArray<string>) {
+    super(fetcher);
+
+    this.domains = domains;
   }
 
   public supports(_ctx: Context, url: URL): boolean {
-    return null !== url.host.match(/vidsrc/);
+    return null !== url.host.match(/vidsrc|vsrc/);
   }
 
-  protected async extractInternal(ctx: Context, url: URL, countryCode: CountryCode): Promise<UrlResult[]> {
-    return this.extractUsingRandomTld(ctx, url, countryCode, [...this.tlds]);
+  protected async extractInternal(ctx: Context, url: URL, meta: Meta): Promise<InternalUrlResult[]> {
+    // While this is a crappy thing to do, they seem to be blocking overly strict IMO
+    const randomIp = `${Math.floor(Math.random() * 223) + 1}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}`;
+    const newCtx = { ...ctx, ip: randomIp };
+
+    return this.extractUsingRandomDomain(newCtx, url, meta, [...this.domains]);
   };
 
-  private async extractUsingRandomTld(ctx: Context, url: URL, countryCode: CountryCode, tlds: string[]): Promise<UrlResult[]> {
-    const tldIndex = Math.floor(Math.random() * tlds.length);
-    const [tld] = tlds.splice(tldIndex, 1) as [string];
+  private async extractUsingRandomDomain(ctx: Context, url: URL, meta: Meta, domains: string[]): Promise<InternalUrlResult[]> {
+    const domainIndex = Math.floor(Math.random() * domains.length);
+    const [domain] = domains.splice(domainIndex, 1) as [string];
 
     const newUrl = new URL(url);
-    const hostnameParts = newUrl.hostname.split('.');
-    hostnameParts[hostnameParts.length - 1] = tld;
-    newUrl.hostname = hostnameParts.join('.');
+    newUrl.hostname = domain;
 
     let html: string;
     try {
-      html = await this.fetcher.text(ctx, newUrl);
+      html = await this.fetcher.text(ctx, newUrl, { queueLimit: 1 });
     } catch (error) {
-      if (error instanceof TooManyRequestsError && tlds.length) {
-        return this.extractUsingRandomTld(ctx, url, countryCode, tlds);
+      /* istanbul ignore next */
+      if (domains.length && (error instanceof TooManyRequestsError || error instanceof BlockedError)) {
+        return this.extractUsingRandomDomain(ctx, url, meta, domains);
       }
 
+      /* istanbul ignore next */
       throw error;
     }
 
-    const $ = cheerio.load(html);
+    const $ = cheerio.load(html.replace('<!--', '').replace('-->', '')); // server HTML is commented-out
 
     const iframeUrl = new URL(($('#player_iframe').attr('src') as string).replace(/^\/\//, 'https://'));
     const title = $('title').text().trim();
 
     return Promise.all(
       $('.server')
-        .map((_i, el) => ({ serverName: $(el).text().trim(), dataHash: $(el).data('hash') }))
+        .map((_i, el) => ({ serverName: $(el).text(), dataHash: $(el).data('hash') }))
         .toArray()
-        /* ओरिजिनल कोड का ढांचा बरकरार है, बस मल्टीपल सर्वर्स की अनुमति दी है */
-        .filter(({ serverName }) => ['CloudStream Pro', 'Hindi', 'Vidsrc', 'Vidplay'].includes(serverName))
+        .filter(({ serverName }) => serverName === 'CloudStream Pro')
         .map(async ({ serverName, dataHash }) => {
-          const iframeHtml = await this.fetcher.text(ctx, new URL(`/rcp/${dataHash}`, iframeUrl.origin), { headers: { Referer: iframeUrl.origin } });
+          const rcpUrl = new URL(`/rcp/${dataHash}`, iframeUrl.origin);
+          const iframeHtml = await this.fetcher.text(ctx, rcpUrl, { headers: { Referer: newUrl.origin } });
           const srcMatch = iframeHtml.match(`src:\\s?'(.*)'`) as string[];
 
-          const playerHtml = await this.fetcher.text(ctx, new URL(srcMatch[1] as string, iframeUrl.origin), { headers: { Referer: iframeUrl.origin } });
-          const fileMatch = playerHtml.match(`file:\\s?'(.*)'`);
-          if (!fileMatch) {
-            throw new NotFoundError();
-          }
-
-          const m3u8Url = new URL(fileMatch[1] as string);
+          const playerHtml = await this.fetcher.text(ctx, new URL(srcMatch[1] as string, iframeUrl.origin), { headers: { Referer: rcpUrl.href } });
+          const fileMatch = playerHtml.match(`(https:\\/\\/.*?{v\\d}.*?) or`) as string[];
+          const m3u8Url = new URL((fileMatch[1] as string).replace(/{v\d}/, iframeUrl.host));
 
           return {
             url: m3u8Url,
             format: Format.hls,
-            label: `${this.label} (${serverName})`,
-            sourceId: `${this.id}_${slugify(serverName)}_${countryCode}`,
-            ttl: this.ttl,
+            label: serverName,
             meta: {
-              countryCodes: [countryCode],
-              height: await guessHeightFromPlaylist(ctx, this.fetcher, m3u8Url),
+              ...meta,
+              height: await guessHeightFromPlaylist(ctx, this.fetcher, m3u8Url, { headers: { Referer: iframeUrl.href } }),
               title,
             },
           };
